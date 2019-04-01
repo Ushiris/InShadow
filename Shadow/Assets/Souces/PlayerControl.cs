09c8452958e4cda532e507c84b2c803d56bf84fc
Unknown <umiushicco@gmail.com>
2019-04-02 01:40:56 +0900

    コメント追加の巻

--- Shadow/Assets/Souces/PlayerControl.cs
+++ Shadow/Assets/Souces/PlayerControl.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-    public float speed;
-    public float JumpTime;
-    public float JumpPower;
-    public Vector3 ShadeGravity;
+    public float speed;         //秒速。
+    public float JumpTime;      //ジャンプのクールタイム
+    public float JumpPower;     //ジャンプ力
+    public Vector3 ShadeGravity;//影内での重力。
 
     bool IsShade = false;
     bool IsJump = false;
@@ -22,9 +22,9 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
         if(IsShade)
         {
+            //操作関係。影の中にいないときは行動できないようになっている。
             if (Input.GetKey(KeyCode.D))
             {
                 transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
@@ -37,15 +37,20 @@ public class PlayerControl : MonoBehaviour {
             {
                 player.AddForce(new Vector3(0, speed / 2.0f));
             }
+
+            //ジャンプの処理。
             if (Input.GetKeyDown(KeyCode.Space) && (!IsJump))
             {
                 player.AddForce(new Vector3(0, JumpPower));
                 IsJump = true;
                 StopWatch = Time.time;
             }
+
+            //影内での重力処理。
             player.AddForce(ShadeGravity, ForceMode.Acceleration);
         }
 
+        //ジャンプのクールタイムの確認
         if(IsJump)
         {
             if (Time.time - StopWatch >= JumpTime)
@@ -56,7 +61,7 @@ public class PlayerControl : MonoBehaviour {
 
     private void OnTriggerStay(Collider other)
     {
-
+        //影内に居る時には通常の重力を消す。
         if (other.transform.tag == "Shadow")
         {
             IsShade = true;
@@ -66,6 +71,7 @@ public class PlayerControl : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
+        //陰に入った段階で一度速度をゼロにする。（そうしないと基本の重力による加速で普通に落ちてしまうため。）
         if (other.transform.tag == "Shadow")
         {
             player.velocity = Vector3.zero;
