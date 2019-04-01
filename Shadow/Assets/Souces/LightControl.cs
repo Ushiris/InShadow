41f255abe43478c5e32b750c625ab8d2ccf87782
Unknown <umiushicco@gmail.com>
2019-04-02 00:18:04 +0900

    ShadowFish（仮）

---
+++ Shadow/Assets/Souces/LightControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+
+    Vector3 moucepos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moucepos = Input.mousePosition;
+
+        //マウスで指し示す場所がワールド座標のz=0であるという意思表示（重要）
+        moucepos.z = -Camera.main.transform.position.z;
+
+        //カーソル君の座標をワールド座標に変換
+        Vector3 mouceWorldPos = Camera.main.ScreenToWorldPoint(moucepos);
+
+        transform.position = mouceWorldPos;
+    }
+}
---
+++ Shadow/Assets/Souces/PlayerControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+    public float JumpTime;
+    public float JumpPower;
+    public Vector3 ShadeGravity;
+
+    bool IsShade = false;
+    bool IsJump = false;
+    float StopWatch;
+    Rigidbody player;
+    
+    
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(IsShade)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += new Vector3(-Time.deltaTime * speed, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                player.AddForce(new Vector3(0, speed / 2.0f));
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && (!IsJump))
+            {
+                player.AddForce(new Vector3(0, JumpPower));
+                IsJump = true;
+                StopWatch = Time.time;
+            }
+            player.AddForce(ShadeGravity, ForceMode.Acceleration);
+        }
+
+        if(IsJump)
+        {
+            if (Time.time - StopWatch >= JumpTime)
+                IsJump = false;
+        }
+
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.transform.tag == "Shadow")
+        {
+            IsShade = true;
+            player.useGravity = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Shadow")
+        {
+            player.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Shadow")
+        {
+            IsShade = false;
+            player.useGravity = true;
+        }
+
+    }
+
+}
